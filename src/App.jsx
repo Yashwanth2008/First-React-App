8da36a7d4d7bcffd09543e06256263e3ba393368
Yashwanth2008 <vyashwanth2003@gmail.com>
2024-08-16 13:49:34 +0530

    general commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,11 @@ import './App.css'
 
 function Nav() {
   return (
-    <div>
-      <nav>
-      <ul className='nav__links'>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">About</a></li>
-      </ul>
-      </nav>
-      <a href="#" className='cta'><button>Contact</button></a>
+    <div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
     </div>
   )
 }
